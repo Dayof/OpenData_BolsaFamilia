acee0bf1cd59f6f8bb621a1dd34cf3233e148e03
Day <dayannefernandesc@gmail.com>
2016-11-27 22:40:39 -0200

    inicio de dev do web service, static, templates e main

---
+++ bf/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# Criando a aplicação Flask
+app = Flask(__name__)
+
+app.config.from_envvar('BF_SETTINGS', silent=True)
+
+print(app.config)
+
+@app.route('/')
+def index():
+    db = getDB()
+    cur = db.execute('select * from PROGRAMA;')
+    entries = cur.fetchall()
+    print entries
+    return render_template('home.html', index_data=entries[0]["CODIGO_PROGRAMA"])
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def getDB():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+if __name__ == '__main__':
+    app.run(debug=True)
