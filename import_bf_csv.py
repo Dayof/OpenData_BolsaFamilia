2bd94642c566c635c397ce823a57c61dd230225b
Day <dayannefernandesc@gmail.com>
2016-11-29 22:21:45 -0200

    alterando formato das tabelas bas

--- import_bf_csv.py
+++ import_bf_csv.py
@@ -1,6 +1,15 @@
 import csv, sqlite3
 
-conn = sqlite3.connect("bf10.db")
+to_db_bf, to_db_fav, to_db_pag = [], [], []
+
+transf = lambda x: int(x.replace(',','')[:-3])
+
+def insertMultTab(u, cs, n, cf, csf, cp, ca, ns, nf, f, v, m):
+    to_db_bf.append((u, cs, n, cf, csf, cp, ca, ns, nf, f, v, m))
+    to_db_fav.append((ns, nf, cs))
+    to_db_pag.append((cp, nf, m, v))
+
+conn = sqlite3.connect("all.db")
 cur = conn.cursor()
 
 print("DROPS NAS TABELAS DE FAVORECIDO E PAGAMENTO...")
@@ -27,41 +36,16 @@ cur.executescript("""
     CREATE TABLE IF NOT EXISTS `FAVORECIDO` (
       `NIS_FAVORECIDO` VARCHAR(15) NOT NULL,
       `NOME_FAVORECIDO` VARCHAR(50) NULL,
-      `MUNICIPIO_CODIGO_SIAFI_MUNICIPIO` VARCHAR(4) NOT NULL,
-      PRIMARY KEY (`NIS_FAVORECIDO`),
-      CONSTRAINT `fk_FAVORECIDO_MUNICIPIO`
-        FOREIGN KEY (`MUNICIPIO_CODIGO_SIAFI_MUNICIPIO`)
-        REFERENCES `MUNICIPIO` (`CODIGO_SIAFI_MUNICIPIO`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION);
+      `CODIGO_SIAFI_MUNICIPIO` VARCHAR(4) NOT NULL,
+      PRIMARY KEY (`NIS_FAVORECIDO`));
 
     CREATE TABLE IF NOT EXISTS `PAGAMENTO` (
-      `PROGRAMA_CODIGO_PROGRAMA` VARCHAR(4) NOT NULL,
-      `FAVORECIDO_NIS_FAVORECIDO` VARCHAR(15) NOT NULL,
-      `MES_COMPETENCIA` VARCHAR(10) NOT NULL,
-      `VALOR_PARCELA` INTEGER NULL,
-      PRIMARY KEY (`PROGRAMA_CODIGO_PROGRAMA`, `FAVORECIDO_NIS_FAVORECIDO`, `MES_COMPETENCIA`),
-      CONSTRAINT `fk_PROGRAMA_has_FAVORECIDO_PROGRAMA1`
-        FOREIGN KEY (`PROGRAMA_CODIGO_PROGRAMA`)
-        REFERENCES `PROGRAMA` (`CODIGO_PROGRAMA`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-      CONSTRAINT `fk_PROGRAMA_has_FAVORECIDO_FAVORECIDO1`
-        FOREIGN KEY (`FAVORECIDO_NIS_FAVORECIDO`)
-        REFERENCES `FAVORECIDO` (`NIS_FAVORECIDO`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION);
-
-    CREATE UNIQUE INDEX `NIS_FAVORECIDO_UNIQUE` ON `FAVORECIDO` (`NIS_FAVORECIDO` ASC);
-
-    CREATE INDEX `fk_FAVORECIDO_MUNICIPIO_idx` ON `FAVORECIDO` (`MUNICIPIO_CODIGO_SIAFI_MUNICIPIO` ASC);
-
-    CREATE INDEX `fk_PROGRAMA_has_FAVORECIDO_FAVORECIDO1_idx` ON `PAGAMENTO` (`FAVORECIDO_NIS_FAVORECIDO` ASC);
-
-    CREATE INDEX `fk_PROGRAMA_has_FAVORECIDO_PROGRAMA1_idx` ON `PAGAMENTO` (`PROGRAMA_CODIGO_PROGRAMA` ASC);
-
+        `COD_PAG` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        `CODIGO_PROGRAMA` VARCHAR(4) NOT NULL,
+        `NIS_FAVORECIDO` VARCHAR(15) NOT NULL,
+        `MES_COMPETENCIA` VARCHAR(10) NOT NULL,
+        `VALOR_PARCELA` INTEGER NOT NULL);
 """)
-
 
 print("CRIAÇÃO DAS TABELAS DE FAVORECIDO E PAGAMENTO...COMPLETO")
 
@@ -93,13 +77,17 @@ CREATE TABLE IF NOT EXISTS FUNCAO_SUBFUNCAO (
 
 print("CRIAÇÃO DAS TABELAS PRIMÁRIAS...COMPLETO")
 
-print("INSERT DO CSV DAS FUNÇÕES...")
+print("INSERT DO CSV DAS FUNÇÕES E SUBFUNCOES...")
 
 with open('func_2015.csv', encoding = "ISO-8859-1") as inf:
     dr = csv.DictReader(inf, delimiter='\t', quoting=csv.QUOTE_NONE)
     print(dr.fieldnames)
     to_db = [(i['CODIGO_FUNCAO'], i['NOME_FUNCAO'],
             i['CODIGO_SUBFUNCAO'], i['NOME_SUBFUNCAO']) for i in dr]
+
+    if not to_db:
+        print("OCORREU ERRO NO PROCESSAMENTO DA CSV DAS FUNCOES E SUBFUNCOES PARA TABELA FUNCAO_SUBFUNCAO...")
+        exit()
 
 cur.executemany("INSERT INTO FUNCAO_SUBFUNCAO ( \
                             CODIGO_FUNCAO, \
@@ -116,37 +104,56 @@ print("INSERT DO CSV DA BOLSA FAMÍLIA...")
 
 print("EXTRAÇÃO NO CSV DA BOLSA FAMILIA...")
 
-transf = lambda x: int(x.replace('.','')[:-3])
-
 with open('bf.csv', encoding = "ISO-8859-1") as inf:
     dr = csv.DictReader(inf, delimiter='\t', quoting=csv.QUOTE_NONE)
     print(dr.fieldnames)
 
-    to_db_bf = [(i['UF'], i['CODIGO_SIAFI_MUNICIPIO'],
-                i['NOME_MUNICIPIO'], i['CODIGO_FUNCAO'],
-                i['CODIGO_SUBFUNCAO'], i['CODIGO_PROGRAMA'],
-                i['CODIGO_ACAO'], i['NIS_FAVORECIDO'],
-                i['NOME_FAVORECIDO'], i['FONTE_FINALIDADE'],
-                transf(i['VALOR_PARCELA']), i['MES_COMPETENCIA']) for i in dr]
+    [insertMultTab(i['UF'], i['CODIGO_SIAFI_MUNICIPIO'],
+    i['NOME_MUNICIPIO'], i['CODIGO_FUNCAO'],
+    i['CODIGO_SUBFUNCAO'], i['CODIGO_PROGRAMA'],
+    i['CODIGO_ACAO'], i['NIS_FAVORECIDO'],
+    i['NOME_FAVORECIDO'], i['FONTE_FINALIDADE'],
+    transf(i['VALOR_PARCELA']), i['MES_COMPETENCIA']) for i in dr]
 
-    to_db_fav = [(i['NIS_FAVORECIDO'], i['NOME_FAVORECIDO'], i['CODIGO_SIAFI_MUNICIPIO']) for i in dr]
+    if not to_db_bf:
+        print("OCORREU ERRO NO PROCESSAMENTO DA CSV DA BF PARA TABELA BOLSA_FAMILIA...")
+        exit()
 
-    to_db_pag = [(i['CODIGO_PROGRAMA'], i['NIS_FAVORECIDO'], i['MES_COMPETENCIA'], transf(i['VALOR_PARCELA'])) for i in dr]
+    if not to_db_fav:
+        print("OCORREU ERRO NO PROCESSAMENTO DA CSV DA BF PARA TABELA FAVORECIDO...")
+        exit()
+
+    if not to_db_pag:
+        print("OCORREU ERRO NO PROCESSAMENTO DA CSV DA BF PARA TABELA PAGAMENTO...")
+        exit()
 
 print("EXTRAÇÃO NO CSV DA BOLSA FAMILIA...COMPLETO")
 
 print("OBJETOS PYTHON PARA DB NA TABELA DE PAGAMENTOS...")
 
-cur.executemany("INSERT OR IGNORE INTO PAGAMENTO ( \
-                                        PROGRAMA_CODIGO_PROGRAMA, \
-                                        FAVORECIDO_NIS_FAVORECIDO, \
-                                        MES_COMPETENCIA, \
-                                        VALOR_PARCELA \
-                                        ) \
-                                        VALUES (?, ?, ?, ?);", to_db_pag)
+cur.executemany("INSERT INTO PAGAMENTO ( \
+                            CODIGO_PROGRAMA, \
+                            NIS_FAVORECIDO, \
+                            MES_COMPETENCIA, \
+                            VALOR_PARCELA \
+                            ) \
+                            VALUES (?, ?, ?, ?);", to_db_pag)
+
 conn.commit()
 
 print("OBJETOS PYTHON PARA DB NA TABELA DE PAGAMENTOS...COMPLETO")
+
+print("OBJETOS PYTHON PARA DB NA TABELA DOS FAVORECIDOS...")
+
+cur.executemany("INSERT OR IGNORE INTO FAVORECIDO ( \
+                                        NIS_FAVORECIDO, \
+                                        NOME_FAVORECIDO, \
+                                        CODIGO_SIAFI_MUNICIPIO \
+                                        ) \
+                                        VALUES (?, ?, ?);", to_db_fav)
+conn.commit()
+
+print("OBJETOS PYTHON PARA DB NA TABELA DOS FAVORECIDOS...COMPLETO")
 
 print("OBJETOS PYTHON PARA DB NA TABELA DO BOLSA FAMILIA...")
 
@@ -171,17 +178,5 @@ cur.executemany("INSERT INTO BOLSA_FAMILIA ( \
 conn.commit()
 
 print("OBJETOS PYTHON PARA DB NA TABELA DO BOLSA FAMILIA...COMPLETO")
-
-print("OBJETOS PYTHON PARA DB NA TABELA DOS FAVORECIDOS...")
-
-cur.executemany("INSERT OR IGNORE INTO FAVORECIDO ( \
-                                        NIS_FAVORECIDO, \
-                                        NOME_FAVORECIDO, \
-                                        MUNICIPIO_CODIGO_SIAFI_MUNICIPIO \
-                                        ) \
-                                        VALUES (?, ?, ?);", to_db_fav)
-conn.commit()
-
-print("OBJETOS PYTHON PARA DB NA TABELA DOS FAVORECIDOS...COMPLETO")
 
 conn.close()
